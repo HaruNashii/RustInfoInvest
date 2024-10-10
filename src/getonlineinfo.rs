433372a0b3435f50923704fabf6cfd519db2df63
haru <parajogapaladins@gmail.com>
2024-10-09 23:54:33 -0300

    reduce code overhead

--- src/getonlineinfo.rs
+++ src/getonlineinfo.rs
@@ -34,7 +34,7 @@ fn clean_string_from_vector(vector_of_string: Vec<String>) -> Vec<String>
         {
             final_string.push_str( &format!("{} ", string) );
         }
-        println!("final string = {}", final_string);
+
         vector_of_clean_strings.push(final_string);
     }
 
@@ -43,29 +43,11 @@ fn clean_string_from_vector(vector_of_string: Vec<String>) -> Vec<String>
 
 
 
-fn parse_to_f64(vector_of_string: Vec<String>) -> f64
+fn parse_to_f64(string: &str) -> f64
 {
-    let mut string_to_parse = String::new();
-
-    for string in vector_of_string
-    {
-        let separeted_string = string.split_whitespace().collect::<Vec<&str>>();
-        let last_string = separeted_string.last().unwrap();
-        println!("testing = {}", last_string);
-        if last_string == &"n/a" 
-        {
-            println!("Fail");
-            continue
-        } 
-        else
-        {
-            println!("Pass");
-            string_to_parse = last_string.replace(",", ".");
-            break;
-        };
-    }
-
-    string_to_parse.parse::<f64>().unwrap()
+    let separeted_string = string.split_whitespace().collect::<Vec<&str>>();
+    let last_string = separeted_string.last().unwrap();
+    last_string.replace(",", ".").parse::<f64>().unwrap()
 }
 
 
@@ -77,13 +59,12 @@ pub async fn infos() -> (Vec<String>, f64)
 {
     // Kill Geckodriver WebDriver to prevent error from the session already being created
     let pkill_command = Command::new("pkill").arg("geckodriver").spawn();
-    std::thread::sleep(Duration::from_millis(1000));
+    std::thread::sleep(Duration::from_millis(500));
     pkill_command.unwrap().kill().unwrap();
    
     // Start the Geckodriver WebDriver
     let web_driver = Command::new("geckodriver").stdout(Stdio::null()).stderr(Stdio::null()).spawn();
-    std::thread::sleep(Duration::from_millis(1000));
-
+    std::thread::sleep(Duration::from_millis(500));
 
 
     // Start the Fantoccini client
@@ -97,10 +78,8 @@ pub async fn infos() -> (Vec<String>, f64)
     let client = ClientBuilder::native().capabilities(caps).connect("http://localhost:4444").await.unwrap();
 
 
-
     // Connect the Fantoccini client to the site of brazil governament that has the Selic tax values
     client.goto("https://www.bcb.gov.br/controleinflacao/historicotaxasjuros").await.unwrap();
-
 
 
     // Get the last 10 Selic tax value
@@ -113,19 +92,17 @@ pub async fn infos() -> (Vec<String>, f64)
         list_elements.push(search_result);
     }
 
-
     
     // Close the Client and the Geckodriver WebDriver
     client.close().await.unwrap();
     web_driver.unwrap().kill().unwrap();
-    println!("Client and WebDriver Finished");
 
 
     // Remove unecessary data from every string in the list_elements vector
     let vector_to_send = clean_string_from_vector(list_elements.clone());
 
     // Parse the most recent Selic tax value to f64
-    let f64_to_send = parse_to_f64(list_elements);
+    let f64_to_send = parse_to_f64(&vector_to_send[0]);
 
     (vector_to_send, f64_to_send) 
 }
