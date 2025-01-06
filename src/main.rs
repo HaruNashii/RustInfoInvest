2962b2883f7550735e9f0e3dc5475fb9bdee3ce0
Haru <parajogapaladins@gmail.com>
2025-01-06 19:38:59 -0300

    prevent page to load while not in their determined rander stage

--- src/main.rs
+++ src/main.rs
@@ -25,16 +25,12 @@ fn main()
     {
         std::thread::sleep(Duration::from_millis(33));
 
-        let main_page = main_page();
-        let realtime_currency_page = realtime_currency_page();
-        let selic_page = selic_page();
         let persistent_page = persistent_page();
-        let investment_wallet_page = investment_wallet_page();
-
         match unsafe{PAGE_TO_RENDER}
         {
             0 => 
             {
+                let main_page = main_page();
                 let mut all_buttons = Vec::new();
                 all_buttons.append(&mut persistent_page.buttons.clone().unwrap());
                 all_buttons.append(&mut main_page.buttons.clone().unwrap());
@@ -45,6 +41,7 @@ fn main()
             },
             1 =>
             {
+                let realtime_currency_page = realtime_currency_page();
                 let mut all_buttons = Vec::new();
                 all_buttons.append(&mut persistent_page.buttons.clone().unwrap());
                 all_buttons.append(&mut realtime_currency_page.buttons.clone().unwrap());
@@ -54,6 +51,7 @@ fn main()
             },
             2 =>
             {
+                let selic_page = selic_page();
                 let mut all_buttons = Vec::new();
                 all_buttons.append(&mut persistent_page.buttons.clone().unwrap());
                 all_buttons.append(&mut selic_page.buttons.clone().unwrap());
@@ -63,6 +61,7 @@ fn main()
             },
             3 =>
             {
+                let investment_wallet_page = investment_wallet_page();
                 handle_input(investment_wallet_page.buttons.clone().unwrap());
                 button_action();
                 render_page(investment_wallet_page, None);
